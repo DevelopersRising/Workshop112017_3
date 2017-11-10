a0fd0cd25f8a7534d9c611c2840cf2a570e30926
Robert Axelsen <rob@rob.ee>
2017-11-10 20:55:10 +0100

    Add iife

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 console.log('HELLO FROM OUR CODE');
 
 var root = document.getElementById('root');
-root.innerHTML = 'SOMETHING ELSE';
+
+(function() {
+    root.innerHTML = 'SOMETHING ELSE';
+})();
